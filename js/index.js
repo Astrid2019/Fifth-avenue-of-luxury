b60120ff952ccb44a151c1958f51c426a8afb937
Aimee <15840958910@163.com>
2019-08-09 17:23:57 +0800

    menu下拉菜单jq代码

--- js/index.js
+++ js/index.js
@@ -1,35 +1,56 @@
 // 导航 我的第五大道下拉
 $("#myFive").mouseover(function () {
     $("#myFiveUl").css("display", "block");
-    $("#myFive").addClass('five_boxShadow');
+    $(this).addClass('five_boxShadow');
 }).mouseout(function () {
     $("#myFiveUl").css("display", "none");
-    $("#myFive").removeClass('five_boxShadow');
+    $(this).removeClass('five_boxShadow');
 })
 
 // 二维码下拉
 $('#phoneDownload').on('mouseover', function () {
-    $("#qr_code").css( "display", "block")
+    $("#qr_code").css("display", "block")
     $(this).addClass('five_boxShadow');
-}).on('mouseout',function(){
+}).on('mouseout', function () {
     $("#qr_code").css("display", "none");
     $(this).removeClass('five_boxShadow');
 })
 
 // 购物车下拉
-$(".shoppingCart").mouseover(function(){
+$(".shoppingCart").mouseover(function () {
     $(".cartNotice").css("display", "inline-block");
     $(".cart_em").css("display", "block");
 })
 $(".shoppingCart").mouseout(function () {
     $(".cartNotice").css("display", "none");
     $(".cart_em").css("display", "none");
-   
+
 })
 
-// 商品分类下拉 右箭头小图标
-// $("#rightArrow").
+// 商品分类下拉 商品分类下拉遮罩层显示
+$("#classify").mouseover(function () {
+    $(".drop_down").css("display", "block");
+    $("#masking").addClass("masking")
+}).mouseout(function () {
+    $(".drop_down").css("display", "none");
+    $("#masking").removeClass("masking")
+})
+$('#rightArrow').on('mouseover', 'li', function () {
+    $(this).css({
+        opacity:1,
+        color: "#c8a985"
+    })
+    $(this).children("section").css("display","block")
+    $(this).children().children().children("i").css("display","inline")
 
+}).on('mouseout', 'li', function () {
+    $(this).css({
+        opacity:.85,
+        color: "#000"
+    })
+    $(this).children("section").css("display","none")
+    $(this).children().children().children("i").css("display","none")
+})
 
 
 

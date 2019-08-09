bdb1dd0e4b424082fd03152082a62169a750e798
Aimee <15840958910@163.com>
2019-08-09 15:05:02 +0800

    上面下拉jq代码修改，menu下拉静态页面

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
 // 导航 我的第五大道下拉
-
 $("#myFive").mouseover(function () {
     $("#myFiveUl").css("display", "block");
     $("#myFive").addClass('five_boxShadow');
-})
-$("#myFive").mouseout(function () {
+}).mouseout(function () {
     $("#myFiveUl").css("display", "none");
     $("#myFive").removeClass('five_boxShadow');
 })
 
 // 二维码下拉
-$("#phoneDownload").mouseover(function(){
-    $("#qr_code").css("display", "block");
-    $("#phoneDownload").addClass('five_boxShadow');
-})
-$("#phoneDownload").mouseout(function () {
+$('#phoneDownload').on('mouseover', function () {
+    $("#qr_code").css( "display", "block")
+    $(this).addClass('five_boxShadow');
+}).on('mouseout',function(){
     $("#qr_code").css("display", "none");
-    $("#phoneDownload").removeClass('five_boxShadow');
+    $(this).removeClass('five_boxShadow');
 })
 
 // 购物车下拉
 $(".shoppingCart").mouseover(function(){
     $(".cartNotice").css("display", "inline-block");
     $(".cart_em").css("display", "block");
-    
 })
 $(".shoppingCart").mouseout(function () {
     $(".cartNotice").css("display", "none");
     $(".cart_em").css("display", "none");
    
 })
+
+// 商品分类下拉 右箭头小图标
+// $("#rightArrow").
 
 
 

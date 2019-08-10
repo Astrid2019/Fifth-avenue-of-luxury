d1d12396043b110d92f816370f21f130e9370558
Aimee <15840958910@163.com>
2019-08-10 10:11:10 +0800

    海外生活hover效果

--- js/index.js
+++ js/index.js
@@ -37,42 +37,49 @@ $("#classify").mouseover(function () {
 })
 $('#rightArrow').on('mouseover', 'li', function () {
     $(this).css({
-        opacity:1,
+        opacity: 1,
         color: "#c8a985"
     })
-    $(this).children("section").css("display","block")
-    $(this).children().children().children("i").css("display","inline")
+    $(this).children("section").css("display", "block")
+    $(this).children().children().children("i").css("display", "inline")
 
 }).on('mouseout', 'li', function () {
     $(this).css({
-        opacity:.85,
+        opacity: .85,
         color: "#000"
     })
-    $(this).children("section").css("display","none")
-    $(this).children().children().children("i").css("display","none")
+    $(this).children("section").css("display", "none")
+    $(this).children().children().children("i").css("display", "none")
 })
 
 // 品牌旗舰
 $('#brandShip').on('mouseover', 'li', function () {
     // $(this).children(".brand_hide").css("top","0")
-    $(this).children().children().children(".brand_hide").css("top","0")
+    $(this).children().children().children(".brand_hide").css("top", "0")
 
 }).on('mouseout', 'li', function () {
-    $(this).children().children().children(".brand_hide").css("top","100px")
+    $(this).children().children().children(".brand_hide").css("top", "100px")
 })
 
 // 热门旗舰店
-$("#hotStore").on("mouseover","section",function(){
-    $(this).children(".hotmiddle").css("opacity","1")
-    $(this).children(".topline").css("width","166px")
-    $(this).children(".bottomline").css("width","166px")
-    $(this).children(".rightline").css("height","85px")
-    $(this).children(".leftline").css("height","85px")
+$("#hotStore").on("mouseover", "section", function () {
+    $(this).children(".hotmiddle").css("opacity", "1")
+    $(this).children(".topline").css("width", "166px")
+    $(this).children(".bottomline").css("width", "166px")
+    $(this).children(".rightline").css("height", "85px")
+    $(this).children(".leftline").css("height", "85px")
 }).on('mouseout', 'section', function () {
-    $(this).children(".hotmiddle").css("opacity","0")
-    $(this).children(".topline").css("width",0)
-    $(this).children(".bottomline").css("width",0)
-    $(this).children(".rightline").css("height",0)
-    $(this).children(".leftline").css("height",0)
-    
+    $(this).children(".hotmiddle").css("opacity", "0")
+    $(this).children(".topline").css("width", 0)
+    $(this).children(".bottomline").css("width", 0)
+    $(this).children(".rightline").css("height", 0)
+    $(this).children(".leftline").css("height", 0)
 })
+
+
+
+
+
+
+
+

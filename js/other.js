41be330a15e0008c0c2b7a92d121c9bf2d1a6f32
Aimee <15840958910@163.com>
2019-08-13 19:24:31 +0800

    注册页面修改

--- js/other.js
+++ js/other.js
@@ -14,13 +14,29 @@ $(".qcode-hook").on("mouseover", function () {
 $("#account").on("click", function () {
     $("#is_qcode_scan").css("display", "none")
     $(".is-hook").css("display", "block")
-    $(this).css("color","#000")
-    $("#scanQrcode").css("color","#999")
+    $(this).css("color", "#000")
+    $("#scanQrcode").css("color", "#999")
 
 })
 $("#scanQrcode").on("click", function () {
     $("#is_qcode_scan").css("display", "block")
     $(".is-hook").css("display", "none")
-    $(this).css("color","#000")
-    $("#account").css("color","#999")
+    $(this).css("color", "#000")
+    $("#account").css("color", "#999")
+})
+
+// 立即注册
+$("#regisLabel").on("click", function () {
+    isTrue = $("#register").hasClass("can-sub");
+    console.log(isTrue)
+    setTimeout(function () {
+        if (!isTrue) {
+        $(".confirmI").css("display", "none");
+        $("#register").addClass("can-sub")
+    } else {
+        $(".confirmI").css("display", "inline-block")
+        $("#register").removeClass("can-sub")
+    }
+    }, 200)
+    
 })
